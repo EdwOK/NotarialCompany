86436b08998529e993a22de72d5086b4f87e08aa
Eduard Paprotski <fisheroliver9@gmail.com>
2016-04-27 01:02:44 +0300

    Remove unused using

--- NotarialCompany/Pages/ClientsPage/ClientDetailsViewModel.cs
+++ NotarialCompany/Pages/ClientsPage/ClientDetailsViewModel.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.Windows.Controls;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
